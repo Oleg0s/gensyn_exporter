4c9b85af028a428e58e6b3ed6eb62939d955b903
oleg0s <oleg.rzm@gmail.com>
2025-11-24 17:33:00 +0200

    Add initial implementation for Gensyn contract interactions, including .gitignore and example scripts.

---
+++ gensyn_contract.py
@@ -0,0 +1,32 @@
+from web3 import Web3
+from GensynABI import ABI
+from config import rpc_url
+from peers import peers
+
+class GensynContract:
+    contract_address = Web3.to_checksum_address('0x7745a8FE4b8D2D2c3BB103F8dCae822746F35Da0')
+    name = "Новый смартконтракт."
+    def __init__(self):
+        self.w3 = Web3(Web3.HTTPProvider(rpc_url))
+        self.contract = self.w3.eth.contract(address=self.contract_address, abi=ABI)
+
+    def getTotalRewards(self, peers):
+        return self.contract.functions.getTotalRewards(peers).call()
+
+    def getTotalWins(self, peer):
+        return self.contract.functions.getTotalWins(peer).call()
+
+class GensynOldContract(GensynContract):
+    contract_address = Web3.to_checksum_address('0xFaD7C5e93f28257429569B854151A1B8DCD404c2')
+    name = "Старый смартконтракт."
+
+if __name__ == "__main__":
+    for c in [GensynOldContract(), GensynContract()]:
+        print(c.name)
+        print("=" * 20)
+        for peer in peers.keys():
+            rewards = c.getTotalRewards(peers[peer])
+            print(peer)
+            for i in range(len(rewards)):
+                print(f" ...{peers[peer][i][-4:]}: {rewards[i]}")
+            print("-" * 20)
---
+++ peers.py
@@ -0,0 +1,24 @@
+peers = {
+    "Oleg0s":
+        [
+            "QmYsuPqJhE4TVDxnPY8DezX1yitCT7pGF2z3DQY29rShZh",
+            "QmYYwER6Tvs6MtP6BCramarS4JLVBViKKmt12C3S1qbzyn",
+            "QmaYGPFSzLQwnFrpRi7gG68qiNmAcDF3obqUs415CXGUHU",
+            "QmUjB9XQXPpjHDqe2CAEuKPaN5tVmgy9vekr37shq4jGnG"
+        ],
+    "Pavel":
+        [
+            "QmNYnogRWuguDo2LaoF9j6Uk5Ck3nJdkmHqTmDXL4WkBFW",
+            "QmcH5PxZPNjDmcYGUHAfWPb75itG8XRXBwLjCupiZAtwnf",
+            "Qmf8YGq769P73hcU7g4CypT8oehSSz8CgJxHvotzmnP2rT",
+            "QmW955auM8jWgFAYQ5N29offQuQj4Fz4adMnkb3ApojmRN",
+            "QmTJA9orV4tAQgjx8YvhGP8VogA5swgS342ZamX7EtPdTg"
+        ],
+    "Alexey":
+        [
+            "QmdKQPVYLwPr8jdDt69PFmag5oouTkfmyfzjqBs691i8Wd"
+        ]
+}
+
+if __name__ == "__main__":
+    pass
