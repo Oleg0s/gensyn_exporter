2948cb3fdcb0ccdc16116f975b309da16abd3d78
oleg0s <oleg.rzm@gmail.com>
2025-11-25 17:37:16 +0200

    Update `run.sh` to include `gensyn-telegram-bot` and adjust container restart sequence

--- run.sh
+++ run.sh
@@ -2,11 +2,14 @@
 
 docker build -t cats/gensyn-exporter .
 
+docker stop gensyn-exporter
+docker rm gensyn-exporter
+docker run -dit --restart always --name gensyn-exporter -p 3437:3437 cats/gensyn-exporter gunicorn -t 120 -w 4 -b 0.0.0.0:3437 exporter:app
+
 docker stop gensyn-scanner
 docker rm gensyn-scanner
 docker run -dit --restart always --name gensyn-scanner cats/gensyn-exporter python ./scanner.py
 
-
-docker stop gensyn-exporter
-docker rm gensyn-exporter
-docker run -dit --restart always --name gensyn-exporter -p 3437:3437 cats/gensyn-exporter gunicorn -t 120 -w 4 -b 0.0.0.0:3437 exporter:app
+docker stop gensyn-telegram-bot
+docker rm gensyn-telegram-bot
+docker run -dit --restart always --name gensyn-telegram-bot cats/gensyn-exporter python ./check_alive.py
