7b8238f84608b4ca0c64f9caabeabfba787f9348
oleg0s <oleg.rzm@gmail.com>
2025-11-30 10:20:05 +0200

    +

--- peers.py
+++ peers.py
@@ -3,7 +3,7 @@ peers = {
         [
             ### EOI через почту: 0xB83385A511fE9fe0b53056c9d01b4e4b673fC4F0
             # "QmYsuPqJhE4TVDxnPY8DezX1yitCT7pGF2z3DQY29rShZh",
-            "QmYYwER6Tvs6MtP6BCramarS4JLVBViKKmt12C3S1qbzyn",
+            # "QmYYwER6Tvs6MtP6BCramarS4JLVBViKKmt12C3S1qbzyn",
             # "QmUjB9XQXPpjHDqe2CAEuKPaN5tVmgy9vekr37shq4jGnG",
             # "QmaYGPFSzLQwnFrpRi7gG68qiNmAcDF3obqUs415CXGUHU", # Bison
 
@@ -11,6 +11,7 @@ peers = {
             "QmSo4fRBiwAKEUoKRceKjSR31ajxPzF3eBWMmBEXTwm13u", #[melodic agile antelope]
             "QmUuWQGRV4RWYCu43MzY5g5d72sRQCDj7gWd1ii33aEzfP", #[webbed savage bear]
             "Qmc7x2E7rm5K71meThdPnEmrm7aVYiBX1e8sC6k3ZaTuQW", #[singing restless tortoise]
+            "QmdWN8BhL1tt4mypkXwvU2YpC9n3QpmMiMYisSq7hxZYBf", #[untamed curious vulture]
         ],
     "Pavel":
         [
@@ -19,6 +20,7 @@ peers = {
             "Qmf8YGq769P73hcU7g4CypT8oehSSz8CgJxHvotzmnP2rT",
             "QmW955auM8jWgFAYQ5N29offQuQj4Fz4adMnkb3ApojmRN",
             "QmTJA9orV4tAQgjx8YvhGP8VogA5swgS342ZamX7EtPdTg",
+            "QmZWAc2Pp1cNVeRF6FMm1xnFgBuTsoayQNbz8CcEg2MPa5",
         ],
     "Alexey":
         [
